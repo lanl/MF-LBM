2e13b5a859334fe9f68d14e70d15e8285b9e8c58
Yu Chen <yu_chen_007@outlook.com>
2022-01-12 19:57:06 +0800

    update makefile release mode with O3 flag; move openacc preprocessor to makefile

--- multiphase_3D/0.src/preprocessor.h
+++ multiphase_3D/0.src/preprocessor.h
@@ -1,8 +1,3 @@
-/*use gpu*/
-!#define _openacc
-/*enable profiling gpu code*/
-!#define gpu_profiling
-
-/*old mrt_bb_opt=1, mrt_ori=2, srt=3, adv_opt=4*/
+/*mrt_bb_opt=1, mrt_ori=2, srt=3, adv_opt=4*/
 #define mrt 2
 
